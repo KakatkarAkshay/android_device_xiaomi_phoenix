c9dc7497cc8e175d693f32861a61d1954f8e0811
Akhil Narang <me@akhilnarang.dev>
2020-04-01 19:52:07 +0530

    phoenix: import CAF powerhal from LA.UM.8.9.r1-08300-SM6xx.0
      * Build it to vendor_overlay
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- device.mk
+++ device.mk
@@ -101,6 +101,17 @@ PRODUCT_PACKAGES += \
     DevicesOverlay \
     DevicesAndroidOverlay
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.ims.xml \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.consumerir.xml \
+    frameworks/native/data/etc/android.hardware.wifi.aware.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.wifi.aware.xml \
+    frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.wifi.rtt.xml
+
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.2-service.phoenix
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
