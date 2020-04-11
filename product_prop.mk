926caf76c5e20ef4e54eeb41346285d52a94bea9
blacksuan19 <i@blacksuan19.me>
2020-04-11 21:44:15 +0800

    phoenix: prop: fix live wallpaper lag
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

--- product_prop.mk
+++ product_prop.mk
@@ -41,3 +41,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     vendor.camera.aux.packagelist=org.lineageos.snap,com.google.android.GoogleCamera,org.codeaurora.snapcam \
     persist.camera.privapp.list=org.codeaurora.snapcam \
     persist.vendor.camera.privapp.list=org.codeaurora.snapcam
+
+# Display
+PRODUCT_PRODUCT_PROPERTIES += \
+	debug.sf.latch_unsignaled=0
